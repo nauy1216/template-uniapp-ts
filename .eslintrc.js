b5cf10c3826e1fdcb55bc199e892421fe6d2e595
liuchengyuan <2267698673@qq.com>
2020-12-18 20:46:51 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/essential", 
+    "plugin:vue/essential",
     "eslint:recommended",
-    "@vue/typescript/recommended", 
-    "@vue/prettier", 
+    "@vue/typescript/recommended",
+    "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
@@ -18,7 +18,7 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-ts-ignore": "off",
-    "max-len": ["off", { code: 500 }]
+    "max-len": ["warn", { code: 100 }]
   },
   overrides: [
     {
